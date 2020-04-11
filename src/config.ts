30e04fe870d310d080363fc1c1ad48e4ab42bdd4
Aivan Monceller <aivan@monceller.com>
2020-04-11 16:32:24 +0800

    chore(name): update package name

--- src/config.ts
+++ src/config.ts
@@ -4,10 +4,11 @@ export default {
   type: Phaser.AUTO,
   parent: 'game',
   backgroundColor: '#33A5E7',
+  pixelArt: false,
   scale: {
     width: 640,
     height: 360,
     mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_BOTH
-  }
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+  },
 };
